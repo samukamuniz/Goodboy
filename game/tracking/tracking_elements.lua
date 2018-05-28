941dbc298f037dbcf367ea9a8bfbde177589fb7a
samukamuniz <samuka.muniz@gmail.com>
2018-05-28 00:17:54 -0300

    Add bases

---
+++ game/tracking/tracking_elements.lua
@@ -0,0 +1,57 @@
+local baseElements = {}
+        
+        baseElements.qtdMoney = 5
+        baseElements.moneys = {}
+        
+            baseElements.moneys[1]={}
+            baseElements.moneys[1].name = "10"
+            baseElements.moneys[1].path = "ui/elements/ten.png"
+            baseElements.moneys[1].value = 10
+            baseElements.moneys[1].type = "money"
+
+            baseElements.moneys[2]={}
+            baseElements.moneys[2].name = "20"
+            baseElements.moneys[2].path = "ui/elements/twenty.png"
+            baseElements.moneys[2].value = 20
+            baseElements.moneys[2].type = "money"
+
+            baseElements.moneys[3]={}
+            baseElements.moneys[3].name = "50"
+            baseElements.moneys[3].path = "ui/elements/fifty.png"
+            baseElements.moneys[3].value = 50
+            baseElements.moneys[3].type = "money"
+
+            baseElements.moneys[4]={}
+            baseElements.moneys[4].name = "100"
+            baseElements.moneys[4].path = "ui/elements/oneHundred.png"
+            baseElements.moneys[4].value = 100
+            baseElements.moneys[4].type = "money"
+
+            baseElements.moneys[5]={}
+            baseElements.moneys[5].name = "bonus"
+            baseElements.moneys[5].path = "ui/elements/moneyBag.png"
+            baseElements.moneys[5].value = 250
+            baseElements.moneys[5].type = "money"
+
+        baseElements.qtdBill = 3
+        baseElements.bills = {}
+
+            baseElements.bills[1]={}
+            baseElements.bills[1].name = "beer"
+            baseElements.bills[1].path = "ui/elements/beer.png"
+            baseElements.bills[1].value = 20
+            baseElements.bills[1].type = "bill"
+
+            baseElements.bills[2]={}
+            baseElements.bills[2].name = "drug"
+            baseElements.bills[2].path = "ui/elements/drug.png"
+            baseElements.bills[2].value = 50
+            baseElements.bills[2].type = "bill"
+
+            baseElements.bills[3]={}
+            baseElements.bills[3].name = "lotery"
+            baseElements.bills[3].path = "ui/elements/lotery.png"
+            baseElements.bills[3].value = 100
+            baseElements.bills[3].type = "bill"
+
+return baseElements
---
+++ game/tracking/tracking_levels.lua
@@ -0,0 +1,43 @@
+local baseLevel = {}
+
+        baseLevel.levels = {}       
+            baseLevel.levels[1] = {}
+                baseLevel.levels[1].sky = "images/screens/sky1.png"
+                baseLevel.levels[1].qtdCity = 2
+                baseLevel.levels[1].city = {}
+
+                    baseLevel.levels[1].city[1] = {}
+                    baseLevel.levels[1].city[1].path = "images/elements/city1.png"
+                    baseLevel.levels[1].city[1].y = 210
+                    
+                    baseLevel.levels[1].city[2] = {}
+                    baseLevel.levels[1].city[2].path = "images/elements/city2.png"
+                    baseLevel.levels[1].city[2].y = 210
+
+            baseLevel.levels[2] = {}
+                baseLevel.levels[2].sky = "images/screens/sky2.png"
+                baseLevel.levels[2].qtdCity = 2
+                baseLevel.levels[2].city = {}
+
+                    baseLevel.levels[2].city[1] = {}
+                    baseLevel.levels[2].city[1].path = "images/elements/city1.png"
+                    baseLevel.levels[2].city[1].y = 210
+                    
+                    baseLevel.levels[2].city[2] = {}
+                    baseLevel.levels[2].city[2].path = "images/elements/city2.png"
+                    baseLevel.levels[2].city[2].y = 210
+
+            baseLevel.levels[3] = {}
+                baseLevel.levels[3].sky = "images/screens/sky3.png"
+                baseLevel.levels[3].qtdCity = 2
+                baseLevel.levels[3].city = {}
+
+                    baseLevel.levels[3].city[1] = {}
+                    baseLevel.levels[3].city[1].path = "images/elements/city1.png"
+                    baseLevel.levels[3].city[1].y = 210
+                    
+                    baseLevel.levels[3].city[2] = {}
+                    baseLevel.levels[3].city[2].path = "images/elements/city2.png"
+                    baseLevel.levels[3].city[2].y = 210
+
+return baseLevel
